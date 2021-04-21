d02feaaa275784abc88793bb9645954057bb1249
Lucas Furlan <lfurlan@technisys.com>
2021-04-21 15:14:54 -0300

    init commit

---
+++ CocktailApp/CocktailApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocktailApp
+//
+//  Created by Lucas Furlan on 21/04/2021.
+//  Copyright © 2021 Lucas Furlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
